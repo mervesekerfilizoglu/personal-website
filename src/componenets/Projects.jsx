140fe48798d3f50d645211960455a227afa0141a
mervesekerfilizoglu <sekermer33@gmail.com>
2025-02-24 18:18:45 +0300

    Projects.jsx üzerinde çalışıldı.

--- src/componenets/Projects.jsx
+++ src/componenets/Projects.jsx
@@ -1,84 +1,86 @@
 import React from "react";
 import "./projects.css";
 const Projects = () => {
-    return (
-      <div className='projects'>
-        <h2 className="projects-title">Projects</h2>
-        <div className="projects-container">
+  return (
+    <div className='projects'>
+      <h2 className="projects-title">Projects</h2>
+      <div className="projects-container">
         <div className="projects-witflix">
-      
-        <img
-          src="src\assets\img\witflix.png"
-          alt="Witflix"
-        />
-            <h4>Witflix</h4>
 
-            <div className="witflix-info">
-          <ul>
-            <li>css</li>
-            <li>html</li>
-            <li>axios</li>
-          </ul>
-        </div>
-
-
-
-            <div className="witflix-links">
-          <a className="btn " href="#">GitHub</a>
-          <a className="btn" href="#">View Site</a>
-        </div>
-            </div>
-           
-          <div className="projects-pizza">
           <img
-          src="src\assets\img\pizza.png"
-          alt="Pizza"
-        />
-            <h4>Pizza</h4>
+            src="src\assets\img\witflix.png"
+            alt="Witflix"
+          />
+          <h4>Witflix</h4>
+          <p4>Developed a sleek and responsive movie streaming web application that allows users to browse, search, and discover movies with a modern, Netflix-inspired UI.</p4>
 
-            <div className="pizza-info">
-          <ul>
-            <li>react</li>
-            <li>axios</li>
-            <li>cypress</li>
-          </ul>
-        </div>
+          <div className="witflix-info">
+            <ul>
+              <li>css</li>
+              <li>html</li>
+              <li>axios</li>
+            </ul>
+          </div>
 
 
 
-            <div className="pizza-links">
+          <div className="witflix-links">
             <a className="btn " href="#">GitHub</a>
-          <a className="btn" href="#">View Site</a>
+            <a className="btn" href="#">View Site</a>
+          </div>
         </div>
+
+        <div className="projects-pizza">
+          <img
+            src="src\assets\img\pizza.png"
+            alt="Pizza"
+          />
+          <h4>Pizza</h4>
+          <p4>Designed and developed a modern pizza ordering web application with a seamless user experience. The app allows users to browse a menu, customize their orders, and streamline the checkout process.</p4>
+
+          <div className="pizza-info">
+            <ul>
+              <li>react</li>
+              <li>axios</li>
+              <li>cypress</li>
+            </ul>
+          </div>
+
+
+
+          <div className="pizza-links">
+            <a className="btn " href="#">GitHub</a>
+            <a className="btn" href="#">View Site</a>
+          </div>
         </div>
         <div className="projects-banking">
-        <img
-          src="src\assets\banking.png"
-          alt="Banking"
-        />
-            <h4>Banking</h4>
-            <div className="banking-info">
-          <ul>
-            <li>react</li>
-            <li>redux</li>
-            <li>hook form</li>
-            <li>axios</li>
-          </ul>
-        </div>
+          <img
+            src="src\assets\banking.png"
+            alt="Banking"
+          />
+          <h4>Banking</h4>
+          <p4>A functional and easy-to-use web application was developed that allows users to control multiple bank accounts through a single application with a modern and functional user interface.</p4>
+          <div className="banking-info">
+            <ul>
+              <li>react</li>
+              <li>redux</li>
+              <li>hook form</li>
+              <li>axios</li>
+            </ul>
+          </div>
 
 
 
-            <div className="banking-links">
+          <div className="banking-links">
             <a className="btn" href="#">GitHub</a>
-          <a className="btn" href="#">View Site</a>
+            <a className="btn" href="#">View Site</a>
+          </div>
         </div>
-        </div>
-     
+
       </div>
       <div className="profile-bottom-line"></div>
-      </div>
-    );
-  };
-  
-  export default Projects;
-  
+    </div>
+  );
+};
+
+export default Projects;
